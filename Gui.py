785821003dee58acfa72693156c40590f130aad1
Marcin Kazmierczak <km48913@zut.edu.pl>
2023-10-17 18:28:43 +0200

    sdsads

--- Gui.py
+++ Gui.py
@@ -18,7 +18,7 @@ class ToolBox(ttk.Frame):
     def open_port_config_window(self):
         self.port_window = tk.Toplevel()
         self.port_window.title("Port Configuration")
-        self.port_window.geometry("600x200")
+        self.port_window.geometry("600x220")
         self.port_frame = PortConfigurationFrame(self.port_window)
 
 
@@ -26,6 +26,16 @@ class PortConfigurationFrame(ttk.Frame):
     """
         Window for port configuration, here we have code to configure and enstablish com connection
     """
+    param_dict = {
+        'Send Timeout': tk.IntVar,
+        'Receive Timeout': tk.IntVar,
+        'Port': tk.StringVar,
+        'Baud Rate': tk.IntVar,
+        'Data Bits': tk.IntVar,
+        'Parity': tk.StringVar,
+        'Stop Bits': tk.IntVar
+    }
+
     def __init__(self, container):
         super().__init__(container)
         options = {'padx': 5, 'pady': 5}
@@ -66,17 +76,26 @@ class PortConfigurationFrame(ttk.Frame):
         self.port_combobox['values'] = ('COM1', 'COM2', 'COM3', 'COM4', 'COM5')
         self.port_combobox.grid(row=0, column=1, **options)
         self.baud_rate_combobox = ttk.Combobox(self.com_options_frame)
-        self.baud_rate_combobox['values'] = ('2400', '4800', '9600', '19200', '115200')
+        self.baud_rate_combobox['values'] = (2400, 4800, 9600, 19200, 115200)
         self.baud_rate_combobox.grid(row=1, column=1, **options)
         self.data_bits_combobox = ttk.Combobox(self.com_options_frame)
-        self.data_bits_combobox['values'] = ('5', '6', '7', '8')
+        self.data_bits_combobox['values'] = (5, 6, 7, 8)
         self.data_bits_combobox.grid(row=2, column=1, **options)
         self.parity_combobox = ttk.Combobox(self.com_options_frame)
         self.parity_combobox['values'] = ('None', 'Even', 'Odd', 'Mark', 'Space')
         self.parity_combobox.grid(row=3, column=1, **options)
         self.stop_bits_combobox = ttk.Combobox(self.com_options_frame)
-        self.stop_bits_combobox['values'] = ('1', '1.5', '2')
+        self.stop_bits_combobox['values'] = (1, 1.5, 2)
         self.stop_bits_combobox.grid(row=4, column=1, **options)
+
+        # buttons for connect
+        self.connect_buttons_frame = tk.Frame(self, highlightbackground="grey", highlightthickness=1)
+        self.connect_buttons_frame.grid(row=3, column=0, columnspan=2)
+        # creating connection button
+        self.connect_button = tk.Button(self.connect_buttons_frame, text="Connect")
+        self.connect_button.grid(row=0, column=0, **options)
+        self.disconnect_button = tk.Button(self.connect_buttons_frame, text="Disconnect")
+        self.disconnect_button.grid(row=0, column=1, **options)
 
         # packing our frame
         self.pack()
